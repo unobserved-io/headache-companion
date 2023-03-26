15cb1f2cbcfbcc19e11bd3d85fd897e412e5a7f2
RIcky Kresslein <ricky@lakoliu.com>
2023-03-26 09:47:50 +0200

    Import data function

--- Migraine/Views/SettingsView.swift
+++ Migraine/Views/SettingsView.swift
@@ -53,7 +53,137 @@ struct SettingsView: View {
                         Text("Export Data")
                     }
                     Button {
-                        
+                        let jsonString = """
+[
+  {
+    "exercise" : 3,
+    "diet" : 3,
+    "notes" : "",
+    "date" : "2023-03-23",
+    "relax" : 2,
+    "attacks" : [
+
+    ],
+    "sleep" : 1,
+    "medications" : [
+
+    ],
+    "water" : 1
+  },
+  {
+    "exercise" : 3,
+    "diet" : 3,
+    "notes" : "",
+    "date" : "2023-03-24",
+    "relax" : 2,
+    "attacks" : [
+
+    ],
+    "sleep" : 3,
+    "medications" : [
+
+    ],
+    "water" : 2
+  },
+  {
+    "exercise" : 2,
+    "diet" : 2,
+    "notes" : "",
+    "date" : "2023-03-25",
+    "relax" : 3,
+    "attacks" : [
+      {
+        "pressing" : false,
+        "id" : "51FF7471-A1A7-47AE-B03D-EA8A7EEA102F",
+        "symptoms" : [
+            "Sensitivity to sound",
+            "Sensitivity to light"
+        ],
+        "pulsating" : false,
+        "otherPainGroup" : 0,
+        "otherPainText" : null,
+        "painLevel" : 2,
+        "stopTime" : 701474555.50431597,
+        "pressingSide" : 0,
+        "headacheType" : 0,
+        "auras" : [
+            "Tingling",
+            "Visual"
+        ],
+        "onPeriod" : false,
+        "pulsatingSide" : 0,
+        "startTime" : 701474487.02191103
+      }
+    ],
+    "sleep" : 2,
+    "medications" : [
+      {
+        "amount" : 2,
+        "dose" : "Pills",
+        "id" : "D5A26714-BEDF-46C7-B211-0BF277E26BE1",
+        "time" : 701467216.79302704,
+        "sideEffects" : null,
+        "effective" : false,
+        "type" : 0,
+        "name" : "Tylenol"
+      }
+    ],
+    "water" : 3
+  }
+]
+"""
+                        if let allDayData = try? JSONSerialization.jsonObject(with: Data(jsonString.utf8), options: .allowFragments) as? [[String: Any]]
+                        {
+                            for hcDayData in allDayData {
+                                let newDay = DayData(context: viewContext)
+                                newDay.date = hcDayData["date"] as? String
+                                newDay.diet = ActivityRanks(rawValue: hcDayData["diet"] as? Int16 ?? 0)!
+                                newDay.exercise = ActivityRanks(rawValue: hcDayData["exercise"] as? Int16 ?? 0)!
+                                newDay.relax = ActivityRanks(rawValue: hcDayData["relax"] as? Int16 ?? 0)!
+                                newDay.sleep = ActivityRanks(rawValue: hcDayData["sleep"] as? Int16 ?? 0)!
+                                newDay.water = ActivityRanks(rawValue: hcDayData["water"] as? Int16 ?? 0)!
+                                newDay.notes = hcDayData["notes"] as! String
+                                if let attacks = hcDayData["attacks"] as? [[String: Any]] {
+                                    for attack in attacks {
+                                        let newAttack = Attack(context: viewContext)
+                                        newDay.addToAttack(newAttack)
+                                        newAttack.id = attack["id"] as? String
+                                        newAttack.headacheType = Headaches(rawValue: attack["headacheType"] as? Int16 ?? 0)!
+                                        newAttack.otherPainGroup = attack["otherPainGroup"] as! Int16
+                                        newAttack.otherPainText = attack["otherPainText"] as? String
+                                        newAttack.painLevel = attack["painLevel"] as! Double
+                                        newAttack.pressing = attack["pressing"] as! Bool
+                                        newAttack.pressingSide = Sides(rawValue: attack["pressingSide"] as? Int16 ?? 0)!
+                                        newAttack.pulsating = attack["pulsating"] as! Bool
+                                        newAttack.pulsatingSide = Sides(rawValue: attack["pulsatingSide"] as? Int16 ?? 0)!
+                                        newAttack.onPeriod = attack["onPeriod"] as! Bool
+                                        newAttack.startTime = attack["startTime"] as? Date
+                                        newAttack.stopTime = attack["stopTime"] as? Date
+                                        if let auras = attack["auras"] as? [String] {
+                                            newAttack.auras = Set(auras)
+                                        }
+                                        if let symptoms = attack["symptoms"] as? [String] {
+                                            newAttack.symptoms = Set(symptoms)
+                                        }
+                                    }
+                                }
+                                if let medications = hcDayData["medications"] as? [[String: Any]] {
+                                    for medication in medications {
+                                        let newMedication = Medication(context: viewContext)
+                                        newDay.addToMedication(newMedication)
+                                        newMedication.id = medication["id"] as? String
+                                        newMedication.amount = medication["amount"] as! Int32
+                                        newMedication.dose = medication["dose"] as? String
+                                        newMedication.effective = medication["effective"] as! Bool
+                                        newMedication.time = medication["time"] as? Date
+                                        newMedication.name = medication["name"] as? String
+                                        newMedication.sideEffects = medication["sideEffects"] as? String
+                                        newMedication.type = MedTypes(rawValue: medication["type"] as? Int16 ?? 0)!
+                                    }
+                                }
+                            }
+                            saveData()
+                        }
                     } label: {
                         Text("Import Data")
                     }
@@ -96,19 +226,6 @@ struct SettingsView: View {
         } message: {
             Text("Are you sure you want to reset all settings? This is irreversible.")
         }
-    }
-    
-    private func loadJson(fileName: String) -> DayData? {
-       let decoder = JSONDecoder()
-       guard
-            let url = Bundle.main.url(forResource: fileName, withExtension: "json"),
-            let data = try? Data(contentsOf: url),
-            let thisDayData = try? decoder.decode(DayData.self, from: data)
-       else {
-            return nil
-       }
-
-       return thisDayData
     }
     
     private func deleteAllData() {
