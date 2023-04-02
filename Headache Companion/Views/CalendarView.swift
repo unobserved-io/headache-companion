196e0cbf888e05b647bad7c6e6dda217c9dce95f
RIcky Kresslein <ricky@lakoliu.com>
2023-04-02 21:41:58 +0200

    Added Attack headacheType to the attacks

--- Headache Companion/Views/CalendarView.swift
+++ Headache Companion/Views/CalendarView.swift
@@ -51,10 +51,14 @@ struct CalendarView: View {
                 Section(refreshIt ? "Attacks" : "Attacks") {
                     ForEach((dayData.first?.attack?.allObjects as? [Attack] ?? []).sorted{$0.wrappedStartTime < $1.wrappedStartTime}) { attack in
                         NavigationLink (destination: AttackView(attack: attack, for: selectedDay).onDisappear() { refreshView() }) {
-                            if attack.stopTime != nil {
-                                Text("\(attack.wrappedStartTime.formatted(date: .omitted, time: .shortened)) - \(attack.wrappedStopTime.formatted(date: .omitted, time: .shortened))")
-                            } else {
-                                Text(attack.wrappedStartTime.formatted(date: .omitted, time: .shortened))
+                            HStack {
+                                if attack.stopTime != nil {
+                                    Text("\(attack.wrappedStartTime.formatted(date: .omitted, time: .shortened)) - \(attack.wrappedStopTime.formatted(date: .omitted, time: .shortened))")
+                                } else {
+                                    Text(attack.wrappedStartTime.formatted(date: .omitted, time: .shortened))
+                                }
+                                Text("(\(headacheTypeString(attack.headacheType)))")
+                                    .foregroundColor(.gray)
                             }
                         }
                     }
