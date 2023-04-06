f720003a23c8aebf84a162c2f87169bc0f907548
RIcky Kresslein <ricky@lakoliu.com>
2023-04-06 18:55:06 +0200

    Group MedHistory into types

--- Headache Companion/Views/MedHistory/MedicationHistoryView.swift
+++ Headache Companion/Views/MedHistory/MedicationHistoryView.swift
@@ -32,63 +32,91 @@ struct MedicationHistoryView: View {
             
             // Preventive
             if medHistory.contains(where: {$0.type == MedTypes.preventive}) {
-                Section {
-                    ForEach(medHistory) { med in
-                        DisclosureGroup(med.name) {
-                            // Start/stop times
-                            if Calendar.current.isDateInToday(med.startDate ?? Date.distantPast) {
-                                Text("Started today")
-                                    .foregroundColor(.gray)
-                            } else {
-                                Text("\(DateFormatter.localizedString(from: med.startDate ?? .now, dateStyle: .short, timeStyle: .none)) to \(med.stopDate != nil ? DateFormatter.localizedString(from: med.stopDate!, dateStyle: .short, timeStyle: .none) : "Present")")
-                                    .foregroundColor(.gray)
-                            }
-                            
-                            // Dose & amount
-                            Text("\(med.amount) x \(med.dose)")
-                                .foregroundColor(.gray)
-                            
-                            // Side effects
-                            if !(med.sideEffects?.isEmpty ?? true) {
-                                HStack {
-                                    Text("Side Effects:")
-                                    Spacer()
-                                    Text(ListFormatter.localizedString(byJoining: med.sideEffects!.sorted { $0 < $1 }.map { $0 as String }))
-                                        .foregroundColor(.gray)
-                                        .multilineTextAlignment(.trailing)
-                                }
-                                .foregroundColor(.gray)
-                            }
-                            
-                            // Effective/ineffective
-                            Text(med.effective ? "Effective" : "Ineffective")
-                                .foregroundColor(.gray)
-                            
-                            // Notes
-                            if !med.notes.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
-                                Text(med.notes)
-                                    .foregroundColor(.gray)
-                            }
-                        }
-                        .swipeActions(edge: .trailing, allowsFullSwipe: true) {
-                            Button("Delete", role: .destructive) {
-                                viewContext.delete(med)
-                                saveData()
-                            }
-                        }
-                        .swipeActions(edge: .leading, allowsFullSwipe: true) {
-                            NavigationLink(
-                                "Edit",
-                                destination: AddEditMedHistoryView(medHistory: med)
-                            )
-                        }
-                    }
-                } header: {
-                    Text("Preventive")
-                } footer: {
-                    medHistory.isEmpty ? nil : Text("Swipe items right to edit, left to delete.")
+                Section("Preventive") {
+                    sectionCreator(medType: MedTypes.preventive)
                 }
                 .listRowBackground(colorScheme == .light ? Color.gray.opacity(0.10) : Color.white.opacity(0.10))
+            }
+            
+            // Symptom Relieving
+            if medHistory.contains(where: {$0.type == MedTypes.symptomRelieving}) {
+                Section("Symptom Relieving") {
+                    sectionCreator(medType: MedTypes.symptomRelieving)
+                }
+                .listRowBackground(colorScheme == .light ? Color.gray.opacity(0.10) : Color.white.opacity(0.10))
+            }
+            
+            // Preventive
+            if medHistory.contains(where: {$0.type == MedTypes.other}) {
+                Section("Other") {
+                    sectionCreator(medType: MedTypes.other)
+                }
+                .listRowBackground(colorScheme == .light ? Color.gray.opacity(0.10) : Color.white.opacity(0.10))
+            }
+        }
+        .toolbar {
+            if !medHistory.isEmpty {
+                ToolbarItemGroup(placement: .bottomBar) {
+                    Text("Swipe items right to edit, left to delete.")
+                        .font(.footnote)
+                        .foregroundColor(.secondary)
+                        .frame(maxWidth: .infinity)
+                }
+            }
+        }
+    }
+    
+    private func sectionCreator(medType: MedTypes) -> some View {
+        return ForEach(medHistory) { med in
+            if med.type == medType {
+                DisclosureGroup(med.name) {
+                    // Start/stop times
+                    if Calendar.current.isDateInToday(med.startDate ?? Date.distantPast) {
+                        Text("Started today")
+                            .foregroundColor(.gray)
+                    } else {
+                        Text("\(DateFormatter.localizedString(from: med.startDate ?? .now, dateStyle: .short, timeStyle: .none)) to \(med.stopDate != nil ? DateFormatter.localizedString(from: med.stopDate!, dateStyle: .short, timeStyle: .none) : "Present")")
+                            .foregroundColor(.gray)
+                    }
+                    
+                    // Dose & amount
+                    Text("\(med.amount) x \(med.dose)")
+                        .foregroundColor(.gray)
+                    
+                    // Side effects
+                    if !(med.sideEffects?.isEmpty ?? true) {
+                        HStack {
+                            Text("Side Effects:")
+                            Spacer()
+                            Text(ListFormatter.localizedString(byJoining: med.sideEffects!.sorted { $0 < $1 }.map { $0 as String }))
+                                .foregroundColor(.gray)
+                                .multilineTextAlignment(.trailing)
+                        }
+                        .foregroundColor(.gray)
+                    }
+                    
+                    // Effective/ineffective
+                    Text(med.effective ? "Effective" : "Ineffective")
+                        .foregroundColor(.gray)
+                    
+                    // Notes
+                    if !med.notes.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+                        Text(med.notes)
+                            .foregroundColor(.gray)
+                    }
+                }
+                .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                    Button("Delete", role: .destructive) {
+                        viewContext.delete(med)
+                        saveData()
+                    }
+                }
+                .swipeActions(edge: .leading, allowsFullSwipe: true) {
+                    NavigationLink(
+                        "Edit",
+                        destination: AddEditMedHistoryView(medHistory: med)
+                    )
+                }
             }
         }
     }
